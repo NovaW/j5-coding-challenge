2289849d048aea06fbc47e462467561fcb0f7ab7
Nova <nova.wiffen@gmail.com>
2021-06-24 19:35:11 +0200

    implemented solution and added NameDataGenerator project for generating test data

--- J5CodingChallenge/Program.cs
+++ J5CodingChallenge/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace J5CodingChallenge
 {
@@ -6,7 +8,47 @@ namespace J5CodingChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var inputFileName = args[0];
+            var outputFileName = args[1];
+            var inputLines = File.ReadAllLines(inputFileName);
+            
+            var maxScorers = GetMaxScorers(inputLines);
+
+            var linesWithSpaces = maxScorers.Select(x => x.FirstName + " " + x.LastName + " " + x.Score.ToString());
+
+            File.Delete(outputFileName);
+            File.WriteAllLines(outputFileName, linesWithSpaces);
         }
+
+        private static List<StudentScore> GetMaxScorers(string[] inputLines){
+            var maxScorers = new List<StudentScore>();
+            int maxScore = 0;
+
+            foreach(var line in inputLines){
+                var splitLine = line.Split(",");
+                var studentScore = new StudentScore{
+                    FirstName = splitLine[0],
+                    LastName = splitLine[1],
+                    Score = int.Parse(splitLine[2])
+                };
+
+                if(studentScore.Score == maxScore){
+                    maxScorers.Add(studentScore);
+                }
+                else if(studentScore.Score > maxScore){
+                    maxScore = studentScore.Score;
+                    maxScorers.Clear();
+                    maxScorers.Add(studentScore);
+                }
+            }
+            return maxScorers;
+        }
+    }
+
+    class StudentScore
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Score { get; set; }
     }
 }
---
+++ NameDataGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace NameDataGenerator
+{
+    class Program
+    {
+        private static string outputFileName = "StudentScores.txt";
+        static void Main(string[] args)
+        {
+            var firstNames = File.ReadAllLines("FirstNames.txt");
+            var lastNames = File.ReadAllLines("LastNames.txt");
+
+            var firstNamesCount = firstNames.Count();
+            var lastNamesCount = lastNames.Count();
+
+            var random = new Random();
+
+            File.Delete(outputFileName);
+            var lines = new List<string>();
+            for(int i = 0; i < 100; i++){
+                var firstName = firstNames[random.Next(0, firstNamesCount)];
+                var lastName = lastNames[random.Next(0, lastNamesCount)];
+                var score = random.Next(0, 100);
+                var line = firstName.Trim() +","+ lastName.Trim()+","+score;
+                lines.Add(line);
+            }
+            File.WriteAllLines(outputFileName, lines);
+
+        }
+    }
+}
