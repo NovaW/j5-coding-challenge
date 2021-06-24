d120a3f803581e8bbb785dc887b6fb20250709ad
Nova <nova.wiffen@gmail.com>
2021-06-24 18:07:06 +0200

    initial commit

---
+++ J5CodingChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace J5CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
